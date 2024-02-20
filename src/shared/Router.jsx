8ebf8c8cbcbb93a961a7e1fd01231bd21c6dd833
기다림 <gidalimforsomeday@gmail.com>
2024-02-20 10:45:08 +0900

    [ADD] 라우터 로그인에 따른 환경추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Detail from "../pages/Detail";
-import Home from "../pages/Home";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import MyPage from "../pages/MyPage/MyPage";
+import Login from "../pages/Login/Login";
+import Home from "../pages/Home/Home";
+import Detail from "../pages/Detail/Detail";
+import { useState } from "react";
 
 
 export const Router = () => {
+
+  const [isLogin, setIsLogin] = useState(false);
+
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
+        {isLogin ? (<>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </>
+        ) : (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate replace to='/' />} />
+          </>)}
       </Routes>
     </BrowserRouter>
   )
